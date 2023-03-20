36ad468796110e274f40ad85d7e7d0e6d180d562
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-20 07:57:13 -0600

    171 - Login

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 export const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(true)
+  const [user, setUser] = useState(false)
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../context/UserContext";
+
 const Home = () => {
+
+  const { user, setUser } = useUserContext()
+
+  const navigate = useNavigate()
+
+  const handleLogin = () => {
+    setUser(true);
+
+    navigate('/dashboard');
+  }
+
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <button onClick={ handleLogin }>Login</button>
+    </>
   );
 }
 
