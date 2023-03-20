a23b08e03db1bb106a8d6ca3444d37d7daf120d0
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-20 07:13:23 -0600

    166 - Navbar

--- src/layout/LayoutRoot.jsx
+++ src/layout/LayoutRoot.jsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const LayoutRoot = () => {
   return <>
-    <nav>NavBar</nav>
+    <Navbar />
     <Outlet />
     <footer>Footer</footer>
   </>
