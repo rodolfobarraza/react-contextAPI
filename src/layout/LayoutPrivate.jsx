59e2e6229a46b3d00c8acf3af13403fc2e8dbd68
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-20 08:04:32 -0600

    172 - Ruta protegida

--- src/layout/LayoutPrivate.jsx
+++ src/layout/LayoutPrivate.jsx
@@ -1,9 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useUserContext } from "../context/UserContext"
 
 const LayoutPrivate = () => {
-    return <>
-        <Outlet />
-    </>
+
+  const { user } = useUserContext()
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+    }
+  }, [ user ])
+
+  return <>
+    <Outlet />
+  </>
   }
   
   export default LayoutPrivate;
