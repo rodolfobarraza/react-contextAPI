318c7049263ab21b83f27aa2e558b50016c77602
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-20 07:23:28 -0600

    168 - createContext

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
 
 const Navbar = () => {
+
+  console.log(useContext(UserContext))
+
   return (
     <nav>
       <NavLink to="/">Home</NavLink> | 
